ac5d3bd18817a24e10478c01f2d230d2e3ab261b
João Victor <86169183+KundeVillanova@users.noreply.github.com>
2022-11-18 01:25:20 -0300

    implementado o token JWT

--- src/main/java/br/com/clients/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/clients/config/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package br.com.clients.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,23 +12,40 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    //classe responsável por gerar os tokens
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${security.jwt.signing-key}")
+    private String signigKey;
+
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        //conversor de token Jwt usando a assintura de signigKey
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(signigKey);
+        return tokenConverter;
+
+    }
+
     @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         //gerar tokens jwt
         endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager);
     }
 
